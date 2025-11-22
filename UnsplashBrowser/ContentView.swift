25f84034bc137e4f0869c591826d75b2668b8117
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2025-11-22 11:30:59 +0100

    init project

---
+++ UnsplashBrowser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnsplashBrowser
+//
+//  Created by Quân Đinh on 22.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UnsplashBrowser/UnsplashBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashBrowserApp.swift
+//  UnsplashBrowser
+//
+//  Created by Quân Đinh on 22.11.25.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
